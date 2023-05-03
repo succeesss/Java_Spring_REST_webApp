4778ed7f4992bfae72ebf755cb8fb721dd99a716
Denis <ahu_ll@mail.ru>
2023-05-03 19:03:30 +0300

    done user login

--- src/main/java/com/example/demo2/HomeController.java
+++ src/main/java/com/example/demo2/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController { // Контроллер для отображения страниц аутентификации
+public class HomeController {
 
     @GetMapping("/login")
     public String login() {
